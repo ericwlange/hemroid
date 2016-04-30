df8c9619110affe03a2112ae2f2e9256c1847f96
Eric Lange <eric@teevos.com>
2016-04-30 12:15:18 +0530

    More fun with ICU makefiles

--- packages/icu/icustub/Android.mk
+++ packages/icu/icustub/Android.mk
@@ -1,11 +1,9 @@
 # icustub
 LOCAL_PATH := $(call my-dir)
-VAULT_PATH := ../../../vault/hemroot/$(TARGET_ARCH_ABI)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        	:= icustub_$(TARGET_ARCH_ABI)
-LOCAL_SRC_FILES     	:= $(VAULT_PATH)/lib/icu/stubdata/libicuhemdata.so
+LOCAL_SRC_FILES     	:= ../../vault/hemroot/$(TARGET_ARCH_ABI)/lib/icu/stubdata/libicuhemdata.so
 LOCAL_EXPORT_CFLAGS 	:= -I$(LOCAL_PATH)/$(VAULT_PATH)/include
 LOCAL_EXPORT_LDLIBS     := -L$(LOCAL_PATH)/$(VAULT_PATH)/lib/icu/stubdata -licuhemdata
 $(LOCAL_MODULE):
@@ -14,7 +12,6 @@ $(LOCAL_MODULE):
 $(LOCAL_MODULE)_JavaInterface:
 	$(if $(findstring ICUData,$(EXPORT_JAVA_INTERFACE)),,$(eval EXPORT_JAVA_INTERFACE += new ICUData(context);))
 $(LOCAL_PATH)/$(LOCAL_SRC_FILES):
-	hemroid install --abi=$(TARGET_ARCH_ABI) icu
 libraries: $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
 
 include $(LOCAL_PATH)/../../hemroid/prebuilt-shared-library.mk
